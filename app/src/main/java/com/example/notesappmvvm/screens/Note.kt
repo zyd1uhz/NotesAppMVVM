014501166af77b489a07287852659cc7a252aa46
190103160@stu.sdu.edu.kz <190103160@stu.sdu.edu.kz>
2022-12-10 17:53:59 +0600

    l1

---
+++ app/src/main/java/com/example/notesappmvvm/screens/Note.kt
@@ -0,0 +1,9 @@
+package com.example.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Note(navController: NavHostController){
+}
